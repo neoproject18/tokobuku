be683f66ff9731e4975cb7afba96aeed927eb59e
dharmabekti <dharmabekti@gmail.com>
2022-08-21 22:26:15 +0700

    Tutorial 38

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,8 +24,8 @@ class App extends BaseConfig
      * @var string
      */
     // public $baseURL = 'http://localhost:8080/';
-    // public $baseURL = 'http://localhost/tokobuku/public/';
-    public $baseURL = BASE_URL;
+     public $baseURL = 'http://localhost/tokobuku/public/';
+    //public $baseURL = BASE_URL;
 
     /**
      * --------------------------------------------------------------------------
